e11da209886408c543401b42fc6d49f23bd6e602
Ivan Starchykov <48174604+starchykov@users.noreply.github.com>
2023-04-26 21:28:16 +0300

    Project configuration

---
+++ ocv_recognizer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ocv_recognizer
+//
+//  Created by Ivan Starchykov on 4/26/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
